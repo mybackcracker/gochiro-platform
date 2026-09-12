20c779b553cebde051375cbd2654f6417c909f85
mybackcracker <contact@mybackcracker.com>
2026-09-11 22:54:37 -0400

    Add Forms page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "/about",
     "/book-online",
     "/contact",
+    "/forms",
     "/philosophy",
     "/pricing",
     "/service-areas",
