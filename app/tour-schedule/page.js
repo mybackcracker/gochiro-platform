fabd50c3a223981b8059f02add567ae1f027a30d
mybackcracker <contact@mybackcracker.com>
2026-09-19 10:17:50 -0400

    Add TourSchedule component with redirect functionality

---
+++ app/tour-schedule/page.js
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function TourSchedule() {
+  redirect('https://script.google.com/macros/s/AKfycbwj-mK2r17ZJDc_0omnY2nlfrGlID2qlbP_QL1cVS99hbbJEhq_4Tm6hlPHP2ETZyso/exec');
+}
